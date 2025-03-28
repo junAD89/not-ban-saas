a4e7a940f67414634135bbd461594d3a5d5b204b
junAD89 <balhighlight@gmail.com>
2025-03-28 21:05:14 +0000

    upadate of pop up component

--- src/app/components/emailPop/page.tsx
+++ src/app/components/emailPop/page.tsx
@@ -1,14 +1,38 @@
 import React, { useState } from 'react'
 import styles from './page.module.css'
 
+import { addDoc, collection } from "firebase/firestore";
+
+import { db } from '../../config/firebase'
 const EmailPopUpCollector = () => {
 
-    const [userEmail, setUserEmail] = useState('')
+    const [userEmail, setUserEmail] = useState('');
+
+
+    // fonction to submit email to firebase firestore
+
+    const handleSubmit = async () => {
+        try {
+            const docRef = await addDoc(collection(db, "emails"), {
+                email: userEmail,
+                createdAt: new Date(),
+            });
+            console.log("doc add");
+
+        } catch (error) {
+            console.error("error while adding doc", error);
+
+
+        }
+    }
+
     return (
         <main>
             <div className={styles.container}>
 
                 <input
+                    value={userEmail}
+                    onChange={(e) => setUserEmail(e.target.value)}
                     className={styles.email_input}
                     placeholder="Enter your email"
                     type="text" />
@@ -16,6 +40,10 @@ const EmailPopUpCollector = () => {
 
 
             <button
+                onClick={
+
+                    () => console.log(userEmail)
+                }
                 className={styles.submit_button}
             >
                 Submit
@@ -24,4 +52,4 @@ const EmailPopUpCollector = () => {
     )
 }
 
-export default EmailPopUpCollector
+export default EmailPopUpCollector;
