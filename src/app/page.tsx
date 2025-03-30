11c23faf8f62b8c05d647a00e892aaa8e46397cb
junAD89 <balhighlight@gmail.com>
2025-03-30 17:45:43 +0000

    add register email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,87 @@
 "use client"
 
+import Link from 'next/link';
 import styles from './page.module.css'
-import EmailPopUpCollector from './components/emailPop/page';
+import { motion } from 'framer-motion';
 
 
 export default function Home() {
 
 
-  const showPopUp = false;
+
+
 
   return (
-    <div>
 
-      <div className={styles.topnav}>
-        <a href="#home"
-        //  className={styles.active}
-        >
-          Not-Ban
-        </a>
-        <div className="choose">
+    <motion.div
+      initial={{ opacity: 0, y: -10 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 0 }}
+      transition={{ duration: 0.8 }}
+    >
+      <div>
 
-          {/* <a href="#">Pricing</a>
-        <a href="#">Review</a>
-        <a href="#">FAQ</a> */}
-        </div>
-
-        <button
-          className={styles.ctaButton}
-        >
-          Get Started
-        </button>
-
-
-
-
-
-
-
-      </div>
-      <div className={styles.hero_section}>
-        <div className={styles.hero_section_principale_text}>
-          <h1>
-            Says goodbye of Reddit Ban
-          </h1>
-
-        </div>
-
-        <div className={styles.hero_section_secondaire_text}>
-          <p>
-            The best way to be not bired of getting banned and wasting your potential
-          </p>
-        </div>
-
-        {showPopUp &&
-          <EmailPopUpCollector />
-
-        }
-
-        <div className={styles.footer_button_container}>
-          <button
-            className={styles.footer_ctaButton}
+        <div className={styles.topnav}>
+          <a href="#home"
+          //  className={styles.active}
           >
-            Get Started
+            Not-Ban
+          </a>
+          <div className="choose">
+
+            {/* <a href="#">Pricing</a>
+  <a href="#">Review</a>
+  <a href="#">FAQ</a> */}
+          </div>
+
+          <button
+
+            className={styles.ctaButton}
+          >
+            <Link href={"registerEmail"}>
+
+              Get Started
+            </Link>
+
           </button>
+
+
+
+
+
+
         </div>
+        <div className={styles.hero_section}>
+          <div className={styles.hero_section_principale_text}>
+            <h1>
+              Says goodbye of Reddit Ban
+            </h1>
+
+          </div>
+
+          <div className={styles.hero_section_secondaire_text}>
+            <p>
+              The best way to be not bired of getting banned and wasting your potential
+            </p>
+          </div>
+
+
+          <div className={styles.footer_button_container}>
+
+            <button
+              className={styles.footer_ctaButton}
+            >
+              <Link href={"registerEmail"}>
+
+                Get Started
+              </Link>
+            </button>
+          </div>
+        </div>
+
+
+
       </div>
-
-
-
-    </div>
+    </motion.div>
   );
 }
---
+++ src/app/registerEmail/page.tsx
@@ -0,0 +1,92 @@
+"use client"
+
+import React, { useState } from 'react'
+import styles from './page.module.css'
+
+import { motion } from "framer-motion";
+
+import { addDoc, collection } from "firebase/firestore";
+
+import { db } from '../config/firebase'
+import { Toaster, toast } from 'sonner';
+const registerEmail = () => {
+
+    const [userEmail, setUserEmail] = useState('');
+    const [error, setError] = useState('');
+
+    const isValidEmail = (email: any) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+    // fonction to submit email to firebase firestore
+
+    const handleSubmit = async () => {
+        try {
+            setError('');
+            // verification if the email is valid
+            if (!isValidEmail(userEmail)) {
+                toast("Please enter a valid email address.");
+                return;
+
+            }
+            try {
+                const docRef = await addDoc(collection(db, "emails"), {
+                    email: userEmail,
+                    createdAt: new Date(),
+                });
+                console.log("doc added with ID:", docRef.id); // Utilisation de docRef ici
+            } catch (error) {
+                console.error("error while adding doc", error);
+            }
+            setUserEmail('');
+            toast("Email added successfully!");
+        } catch (error) {
+            console.error("Error adding document: ", error);
+        }
+    }
+
+    return (
+        <motion.div
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3 }}
+        >
+            <Toaster position="top-center" />
+
+            <main className={styles.allContainer}>
+
+                <div className={styles.topText}>
+                    Enter Your emails to join beta user and get more features in preview
+                </div>
+                <div className={styles.container}>
+
+                    <div >
+
+                        <input
+                            value={userEmail}
+                            onChange={(e) => setUserEmail(e.target.value)}
+                            className={styles.email_input}
+                            placeholder="Enter your email"
+                            type="email" />
+                    </div>
+
+
+                    <button
+                        onClick={
+
+                            () => {
+                                console.log(userEmail);
+                                handleSubmit();
+                            }
+                        }
+                        className={styles.submit_button}
+                    >
+                        Submit
+                    </button>
+                </div>
+            </main >
+        </motion.div>
+    )
+}
+
+export default registerEmail;
